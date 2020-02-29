40b5cad012116848848e7c62da2a15dae3507da3
Alexa Dolby <59667085+alexadolby@users.noreply.github.com>
2020-02-28 21:53:11 -0500

    README update and robo updates

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 [Project Description]
 (https://github.com/prof-rossetti/intro-to-python/tree/master/projects/robo-advisor)
 
+## Prerequisites 
+
+    + Anaconda 3.7
+    + Python 3.7
+    + Pip
+
 ## Installation
 
 Clone or download from [GitHub source] then navigate into the project repository 
@@ -10,6 +16,22 @@ Clone or download from [GitHub source] then navigate into the project repository
 ```sh
 cd roboadvisor
 ```
+
+Use Anaconda to create and activate a new virtual enviornment. From inside this enviornment, install the package dependencies:
+
+```sh
+pip install requests python-dotenv
+```
+## Setup 
+
+Before using or developing this application, take a moment to obtain an AlphaVantage API Key (e.g. "abc123").
+
+After obtaining an API Key, create a new file in this repository called ".env", and update the contents of the ".env" file to specify your real API Key:
+
+```sh
+API_KEY="abc1212"
+```
+
 ## Usage 
 
 Run the program 
--- robo.py
+++ robo.py
@@ -1,18 +1,87 @@
 # app/robo_advisor.py
 
+import csv
+import json
+import os
+
+from dotenv import load_dotenv
+import requests
+import datetime 
+load_dotenv()
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
+api_key = os.envioron.get("API_KEY")
+
+symbol = "MSFT" # accept user input
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
+
+response = requests.get(request_url)
+
+parsed_response = json.loads(response.text)
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys())
+
+latest_day = dates[0]
+
+
+latest_close = tsd[latest_day]["4. close"]
+high_prices = []
+low_prices = []
+
+for date in dates:
+    high_price = tsd[date]["2. high"]
+    low_price = tsd[date]["3. low"]
+    high_prices.append(float(high_price))
+    low_prices.append(float(low_price))
+
+recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+
+#csv_file_path = "data/prices.csv" # a relative filepath
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader() # uses fieldnames set above
+    for date in dates:
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
+        })
+
+
+
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print("SELECTED SYMBOL: MSFT")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+now = datetime.datetime.now()
+print("REQUEST AT: " + now.strftime("%Y-%m-%d, %I:%M %p")) #https://www.programiz.com/python-programming/datetime/strftimeprint("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path}")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
